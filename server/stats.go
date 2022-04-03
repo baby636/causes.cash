370c7d759c9a66213d1eabed1b6450d666fd6c0b
kristof kaehler <61610729+baby636@users.noreply.github.com>
2022-04-03 19:20:15 +0200

    Initialize snyk-fix-95cfee1a16ce6ee21aedbaf9db9ced9a

---
+++ server/stats.go
@@ -0,0 +1,76 @@
+package server
+
+// StatsInformer defines an interface that exposes tracker stats
+type StatsInformer interface {
+	Stats(string, bool) *TrackerStats
+}
+
+// TrackerStats represents a snapshot of the trackers statistics
+type TrackerStats struct {
+	BanScore             uint32      `json:"banScore"`
+	Banned               bool        `json:"banned"`
+	Connections          int         `json:"connections"`
+	PoolSize             int         `json:"poolSize"`
+	Pools                []PoolStats `json:"pools"`
+	ShufflePort          int         `json:"shufflePort"`
+	ShuffleWebSocketPort int         `json:"shuffleWebSocketPort"`
+}
+
+// PoolStats represents the stats for a particular pool
+type PoolStats struct {
+	Members int    `json:"members"`
+	Amount  uint64 `json:"amount"`
+	Type    string `json:"type"`
+	Full    bool   `json:"full"`
+	Version uint64 `json:"version"`
+}
+
+// Stats returns the tracker stats.
+func (t *Tracker) Stats(ip string, tor bool) *TrackerStats {
+	t.mutex.RLock()
+	defer t.mutex.RUnlock()
+
+	banned := false
+	var banScore uint32
+
+	data := t.banData[ip]
+	if data != nil {
+		banScore = data.score
+		if data.score >= maxBanScore {
+			banned = true
+		}
+	}
+
+	sp := t.shufflePort
+	if tor {
+		sp = t.torShufflePort
+	}
+
+	wssp := t.shuffleWebSocketPort
+	if tor {
+		wssp = t.torShuffleWebSocketPort
+	}
+
+	ts := &TrackerStats{
+		BanScore:             banScore,
+		Banned:               banned,
+		Connections:          len(t.connections),
+		PoolSize:             t.poolSize,
+		Pools:                make([]PoolStats, 0),
+		ShufflePort:          sp,
+		ShuffleWebSocketPort: wssp,
+	}
+
+	for _, p := range t.pools {
+		ps := PoolStats{
+			Members: p.PlayerCount(),
+			Amount:  p.amount,
+			Type:    p.shuffleType.String(),
+			Full:    p.IsFrozen(),
+			Version: p.version,
+		}
+		ts.Pools = append(ts.Pools, ps)
+	}
+
+	return ts
+}
